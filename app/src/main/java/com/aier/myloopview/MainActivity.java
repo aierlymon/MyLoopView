9c4085afab2a57143d6eb1de363677029be51a96
huanghao <huanghao@12dong.cn>
2018-10-29 18:32:46 +0800

    完成了轮播的viewpager

--- app/src/main/java/com/aier/myloopview/MainActivity.java
+++ app/src/main/java/com/aier/myloopview/MainActivity.java
@@ -1,13 +1,59 @@
 package com.aier.myloopview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.aier.myloopview.widgets.LoodView;
+import com.facebook.drawee.backends.pipeline.Fresco;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoodView mLoodView;
+    private PermissionHelper mPermissionHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         setContentView(R.layout.activity_main);
+        mLoodView = ((LoodView) findViewById(R.id.loopview));
+        mPermissionHelper = new PermissionHelper(this);
+        if (Build.VERSION.SDK_INT < 23) {
+            // 如果系统版本低于23，直接跑应用的逻辑
+            Log.i("tag", "The api level of system is lower than 23, so run app logic directly.");
+        } else {
+            // 如果权限全部申请了，那就直接跑应用逻辑
+            if (mPermissionHelper.isAllRequestedPermissionGranted()) {
+                Log.i("tag", "All of requested permissions has been granted, so run app logic directly.");
+            } else {
+                // 如果还有权限为申请，而且系统版本大于23，执行申请权限逻辑
+                Log.i("tag", "Some of requested permissions hasn't been granted, so apply permissions first.");
+                mPermissionHelper.applyPermissions();
+            }
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mPermissionHelper.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mPermissionHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLoodView.onDestroy();
     }
 }
